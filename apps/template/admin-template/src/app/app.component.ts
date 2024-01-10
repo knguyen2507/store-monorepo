202eeb536ba9d57f0a03a99f3c4bc742b04569dc
knguyen2507 <127070630+knguyen2507@users.noreply.github.com>
2024-01-10 20:13:47 +0700

    fix login navigation

--- apps/template/admin-template/src/app/app.component.ts
+++ apps/template/admin-template/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  showComponent = true;
+
+  constructor(private router: Router) {
+    this.router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url === '/dang-nhap') {
+          this.showComponent = false;
+        }
+      }
+    });
+  }
+
   ngOnInit(): void {
     return;
   }
